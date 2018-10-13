dca7b2ce44ad525eb20b4f6e549add84e1d554c2
Chris Davis <chrisdavis@cdavismac.attlocal.net>
2018-10-12 22:54:42 -0400

    Homepage layout updates

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.PNG';
 import './App.css';
 
+const textColor = {
+    color: 'red'
+};
+
+
 class App extends Component {
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
+          <p style={textColor}>
+             Welcome to The Roof Docs
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
