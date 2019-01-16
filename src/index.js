90228dbc1b104fbf08e456c06c9d42c4ba1b70f7
Chris Davis <chrisdavis18@comcast.net>
2019-01-15 22:43:18 -0500

    Service Worker update

--- src/index.js
+++ src/index.js
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom';
 import './style/index.css';
 import './style/components/navigation.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { unregister } from './serviceWorker.js';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Force Reload of App
+unregister();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
