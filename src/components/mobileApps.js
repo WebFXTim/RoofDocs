e200b41f21ce00004c83ab8681e4210bbf321ad9
Chris Davis <chrisdavis18@comcast.net>
2019-03-27 14:46:28 -0400

    Add adjuster meeting app link

--- src/components/mobileApps.js
+++ src/components/mobileApps.js
@@ -14,6 +14,7 @@ class MobileApps extends Component {
                 <p className="pageHeader"> Mobile Apps </p>
                 <a href="itms-services://?action=download-manifest&url=https://s3.amazonaws.com/theroofdocs.ios/theroofdocs.plist"><p className="appTitle">Download Sales App</p></a>
                 <a href="itms-services://?action=download-manifest&url=https://s3.amazonaws.com/theroofdocs.ios/ProjectMeeting.plist"><p className="appTitle">Download Project Meeting App</p></a>
+                <a href="itms-services://?action=download-manifest&url=https://s3.amazonaws.com/theroofdocs.ios/AdjusterMeeting.plist"><p className="appTitle">Download Adjuster Meeting App</p></a>
             </div>
 
         );
