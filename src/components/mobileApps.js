ae135d0d27680cf6f00f425a8b6713eca459e0b5
Chris Davis <chrisdavis18@comcast.net>
2019-04-23 16:11:58 -0400

    Add field portal to list

--- src/components/mobileApps.js
+++ src/components/mobileApps.js
@@ -15,7 +15,7 @@ class MobileApps extends Component {
                 <a href="itms-services://?action=download-manifest&url=https://s3.amazonaws.com/theroofdocs.ios/theroofdocs.plist"><p className="appTitle">Download Sales App</p></a>
                 <a href="itms-services://?action=download-manifest&url=https://s3.amazonaws.com/theroofdocs.ios/ProjectMeeting.plist"><p className="appTitle">Download Project Meeting App</p></a>
                 <a href="itms-services://?action=download-manifest&url=https://s3.amazonaws.com/theroofdocs.ios/AdjusterMeeting.plist"><p className="appTitle">Download Adjuster Meeting App</p></a>
-                <a href="itms-services://?action=download-manifest&url=https://s3.amazonaws.com/theroofdocs.ios/WrapUp.plist"><p className="appTitle">Download Project Wrap Up App</p></a>
+                <a href="itms-services://?action=download-manifest&url=https://s3.amazonaws.com/theroofdocs.ios/FieldApp.plist"><p className="appTitle">Download Field Portal App</p></a>
             </div>
 
         );
