3a6e840314e0d18f0c3c9384c8b2948d2f0f24fb
Chris Davis <chrisdavis18@comcast.net>
2019-12-09 13:54:04 -0500

    IE Support

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './style/index.css';
 import App from './App';
 import { unregister } from './serviceWorker.js';
+import { polyfill } from 'es6-promise'; polyfill();
 
 // Force Reload of App
 unregister();
