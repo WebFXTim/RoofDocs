bb6eabd19523fade5fa4af64d4212a805eb28d1e
Chris Davis <chrisdavis18@comcast.net>
2019-12-09 15:11:54 -0500

    Try to add ES6-Promise

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
+import { polyfill } from 'es6-promise';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style/index.css';
 import App from './App';
 import { unregister } from './serviceWorker.js';
-import { polyfill } from 'es6-promise'; polyfill();
+polyfill();
 
 // Force Reload of App
 unregister();
